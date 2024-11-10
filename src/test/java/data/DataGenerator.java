9016faefd573adc2ee06263089b08975526c117c
Sergey Bykhavets <sergozby@gamil.com>
2024-11-10 23:37:15 +0300

    Change jar to proper version, adjust test

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -2,7 +2,6 @@ package data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -19,19 +18,19 @@ public class DataGenerator {
 
     public static String generateCity() {
         String[] cities = new String[]{
-                "Москва", "Уфа", "Казань","Саратов", "Якутск"
+                "Москва", "Уфа", "Казань", "Саратов", "Якутск"
         };
         return cities[new Random().nextInt(cities.length)];
     }
 
     public static String generateName(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        return faker.name().lastName() + " " + faker.name().lastName();
+        return faker.name().lastName() + " " + faker.name().firstName();
     }
 
     public static String generatePhone(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        return faker.phoneNumber().phoneNumber();
+        return faker.phoneNumber().phoneNumber().replace('(', ' ').replace(')', ' ').replace('-', ' ').replaceAll("\\s", "");
     }
 
     public static class Registration {
@@ -39,64 +38,14 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
-            return new UserInfo(generateCity(),generateName(locale),generatePhone(locale));
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
     }
 
     @Value
-    public static final class UserInfo {
-        private final String city;
-        private final String name;
-        private final String phone;
-
-        public UserInfo(String city, String name, String phone) {
-            this.city = city;
-            this.name = name;
-            this.phone = phone;
-        }
-
-        public String getCity() {
-            return this.city;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-
-        public String getPhone() {
-            return this.phone;
-        }
-
-        public boolean equals(final Object o) {
-            if (o == this) return true;
-            if (!(o instanceof UserInfo)) return false;
-            final UserInfo other = (UserInfo) o;
-            final Object this$city = this.getCity();
-            final Object other$city = other.getCity();
-            if (this$city == null ? other$city != null : !this$city.equals(other$city)) return false;
-            final Object this$name = this.getName();
-            final Object other$name = other.getName();
-            if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
-            final Object this$phone = this.getPhone();
-            final Object other$phone= other.getPhone();
-            if (this$phone == null ? other$phone != null : !this$phone.equals(other$phone)) return false;
-            return true;
-        }
-
-        public int hashCode() {
-            final int PRIME = 59;
-            int result = 1;
-            final Object $city = this.getCity();
-            result = result * PRIME + ($city == null ? 43 : $city.hashCode());
-            final Object name = this.getName();
-            result = result * PRIME + (name == null ? 43 : name.hashCode());
-            final Object phone = this.getPhone();
-            result = result * PRIME + (phone == null ? 43 : phone.hashCode());
-            return result;
-        }
-
-        public String toString() {
-            return "DataGenerator.UserInfo(city=" + this.getCity() + ", name=" + this.getName() + ", phone=" + this.getPhone() + " )";
-        }
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
     }
 }
